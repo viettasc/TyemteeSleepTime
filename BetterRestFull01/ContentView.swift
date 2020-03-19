73ac6019d377da37ebcdf9492f8178fa4f802dac
viettasc <51367503+viettasc@users.noreply.github.com>
2020-03-19 15:19:51 +0700

    Initial Commit

---
+++ BetterRestFull01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRestFull01
+//
+//  Created by Viettasc Doan on 3/19/20.
+//  Copyright © 2020 Viettasc Doan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
